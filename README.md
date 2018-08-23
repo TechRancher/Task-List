1b47ddd91dd4740407e01fe171a9a447d488a182
Jason Sikes <sikes.jay@gmail.com>
2018-08-22 21:29:52 -0500

    Set up basic task form and appended elements through JS

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Task-List
+
+This is a simple Task list app that allows you to add a task. Save it to local storage and remove it if you want.
+This app uses Materialize CSS, Font Awesome, and jQuery for the Materialize. All the functions have been written in JavaScript.
+
+Materialize CSS Release: 1.0.0-rc.2 https://materializecss.com
+Font Awesome Release: 5.2.0 https://fontawesome.com
+jQuery Release: 3.3.1 - uncompressed  https://code.jquery.com
---
+++ js/app.js
@@ -0,0 +1,50 @@
+// Define UI Vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-task");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === ""){
+    alert("Add a task");
+  }
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class 
+  link.className = "delete-item secondary-content";
+  // Add icon htmlaria-hidden="true"
+  link.innerHTML = `<i class="far fa-trash-alt"></i>`;
+  // Append the link to li
+  li.appendChild(link);
+  // Append the li to ul
+  taskList.appendChild(li);
+  // Clear input
+  taskInput.value = "";
+  // Prevent Default
+  e.preventDefault();
+  console.log(li);
+}
+
+
+
+
+
+
+
