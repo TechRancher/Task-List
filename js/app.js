efea833f722cb5693d641e4fa28184222644e180
Jason Sikes <sikes.jay@gmail.com>
2018-08-29 23:29:12 -0500

    Code functions to Btn and remove li

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Define UI Vars
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-task");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
@@ -12,12 +12,22 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  // Remove Task event
+  taskList.addEventListener("click", removeTask);
+  // Clear Task event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add Task
 function addTask(e) {
   if(taskInput.value === ""){
-    alert("Add a task");
+    if(confirm("You must add something to the list before submit.")) {
+      removeEmpty();
+    } else {
+      removeEmpty();
+    }
   }
   // Create li element
   const li = document.createElement("li");
@@ -39,12 +49,43 @@ function addTask(e) {
   taskInput.value = "";
   // Prevent Default
   e.preventDefault();
-  console.log(li);
+  
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains("delete-item")) {
+    if(confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Task
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
 
 
 
-
+function removeEmpty() {
+  taskList.removeChild(taskList.lastChild);
+}
 
 
 
